bf87e0bd15008c62b6521a9bfc88a6dacf7bcdf8
with <contact@fleur.is>
2023-11-06 22:43:03 +0100

    add (research) -> added pof of hex print

--- research/generic_playground.c
+++ research/generic_playground.c
@@ -3,6 +3,9 @@
 #include "seul/ints.h"
 #include "seul/allocators.h"
 
+
+void* stdout = (void*)1;
+
 struct cool_s {
 	i32 a;
 	void* c;
@@ -11,6 +14,42 @@ struct cool_s {
 
 
 // NOTE: this code run on both open bsd and windows
+
+
+struct res_s {
+	u8 res[64];
+	u8 len;
+};
+// NOTE: just testing, not real impl
+struct res_s print_uint_as_hex(usize a, u8 len) {
+
+	struct res_s r = (struct res_s){ 0 };
+
+	r.res[0] = '0';
+	r.res[1] = 'x';
+
+	for (i8 gro = 0; gro < len ; gro ++ ) {
+
+		u8 assci_base = 48;
+		u8 lsb = gro*4;
+		u8 msb = gro*4+4;
+
+		u8 group = (a >> lsb) & ~(~0 << (msb-lsb));
+		
+		if ( group > 9 ) {
+			assci_base = 87;
+		}
+
+		u8 cha = assci_base+group;
+		r.res[(len+1)-gro] = cha;
+		
+	}
+
+	seul_platform_write(stdout, &r.res, len+2);
+	seul_platform_write(stdout, "\n", 1);
+	
+	return r;
+};
 
 int _start() {
 
@@ -21,7 +60,6 @@ int _start() {
 	// isize b = seul_count_of_static_array( (i32[10]){} );
 
 	const char* text = "hello_openbsd\n";
-	void* stdout = (void*)1;
 
 	#if __target_windows	
 	stdout = seul_platform_windows_get_peb()->process_parameters->standard_output;
@@ -32,12 +70,15 @@ int _start() {
 	seul_platform_write(stdout, ":)\n", 3);
 
 
-	struct seul_allocator_mem_alloc_optional_s r = seul_platform_alloc_virtual_memory(0, 5000, saop_read);
+	struct seul_allocator_mem_alloc_optional_s r = seul_platform_alloc_virtual_memory(0, 5000, saop_read | saop_write);
 	if (r.state == saor_ok) {
 		seul_platform_write(stdout, "yes\n", 4);
 	} else {
 		seul_platform_write(stdout, "no\n", 3);
 	}
+
+	*(u8*)(r.address) = 123;
+	print_uint_as_hex((usize)r.address, seul_size_of(void*)*2);
 
 	seul_platform_exit(0);
 	return 1;
